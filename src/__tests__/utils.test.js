17623149b6453a10edf42172bb07396d64128520
Jared Patterson <jpwesley34@gmail.com>
2024-02-21 09:26:18 -0800

    Follows along.

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,13 @@
-// Your tests here
+//I want words to get points. Vowels get 1, consonants get two. In other words, "I expect calling the function pointsForWord("test") to return 7."
+
+import {pointsForWord} from "../utils";
+
+describe("pointsForWord", () => {
+    it("handles uppercase and lowercase input", () => {
+        const word = "tEsT";
+      
+        const points = pointsForWord(word);
+      
+        expect(points).toBe(7);
+      });
+  });
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,27 @@
-// Your code here
+// export function pointsForWord(word) {
+//     let points = 0;
+//     for (const char of word) {
+//       if (["a", "e", "i", "o", "u"].includes(char)) {
+//         points += 1;
+//       } else {
+//         points += 2;
+//       }
+//     }
+//     return points;
+//   } ORIGINAL VERSION
+
+// export function pointsForWord(word) {
+//     let points = 0;
+//     for (const char of word) {
+//       points += /[aeiou]/.test(char) ? 1 : 2;
+//     }
+//     return points;
+//   } //Better version I know less about. Uses "regular expression to check for vowels."
+
+export function pointsForWord(word) {
+    let points = 0;
+    for (const char of word) {
+      points += /[aeiou]/i.test(char) ? 1 : 2; //i flag is here.
+    }
+    return points;
+  } //Even newer version, uses "(in this case, using the i flag for the regular expression to indicate a case-insensitive match for either uppercase or lowercase values)"
